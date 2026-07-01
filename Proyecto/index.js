21597fab4b1319de913dca449880fbe101bc5841
Enriquejm1 <best_john@hotmail.com>
2026-07-01 15:25:45 -0600

    Configuracion del servidor correctamente

---
+++ Proyecto/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const debug = require('debug')('app:server');
+const {Config} = require('./src/config');
+
+const app = express();
+app.use(express.json());
+
+//Modulos
+
+app.listen(Config.port,()=>{
+    debug(`Servidor escuchando 1 ${Config.port}`);
+    //console.log(process.env.debug)
+    //console.log(`Servidor escuchando 2 ${Config.port}`);
+});
