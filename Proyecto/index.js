552a79528b0cdbf94aafe754b53ceeb4fd5e2cec
Enriquejm1 <best_john@hotmail.com>
2026-07-01 21:51:42 -0600

    Se agrego el de usuarios

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const debug = require('debug')('app:server');
 const {Config} = require('./src/config/index');
 const {ProductsAPI} = require("./src/products/index")
-
+const {UserAPI}=require('./src/users/index');
 const app = express();
 app.use(express.json());
 
 //Modulos
 ProductsAPI(app);
-
+UserAPI(app);
 app.listen(Config.port,()=>{
     debug(`Servidor escuchando 1 ${Config.port}`);
     //console.log(process.env.debug)
