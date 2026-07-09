6ecb6ccba5adef02fcc58b0c0f91da3a0e11d12e
Enriquejm1 <best_john@hotmail.com>
2026-07-08 21:37:24 -0600

    Proyectos y menu terminado

--- Proyectos-JavaScript/Calculadora_BMI/js/script.js
+++ Proyectos-JavaScript/Calculadora_BMI/js/script.js
@@ -3,10 +3,24 @@ function calculaBMI(){
         const height = parseFloat(document.getElementById("height").value)/100;
         if(weight && height){
             const bmi = (weight/(height*height)).toFixed(2);
+            if(bmi<25 && bmi>18){
             document.getElementById("result").style.color="green";
-            document.getElementById("result").textContent = `BMI es: ${bmi}`;
+            document.getElementById("result").textContent = `BMI es ${bmi} segun el peso y la estatura, Estas Saludable`;    
+            }else if(bmi>=25 && bmi<30){
+            document.getElementById("result").style.color="blue";
+            document.getElementById("result").textContent = `BMI es ${bmi} segun el peso y la estatura, Estas en sobrepeso consulta a un profesional de la salud.`;        
+            }else if(bmi>=30 && bmi<40){
+            document.getElementById("result").style.color="orange";
+            document.getElementById("result").textContent = `BMI es ${bmi} segun el peso y la estatura, Estas en obesidad consulta a un profesional de la salud.`;        
+            }else if(bmi>=40){
+            document.getElementById("result").style.color="red";
+            document.getElementById("result").textContent = `BMI es ${bmi} segun el peso y la estatura, Estas en obesidad 2 consulta a un profesional de la salud.`;        
+            }else{
+            document.getElementById("result").style.color="gray";
+            document.getElementById("result").textContent = `BMI es ${bmi} segun el peso y la estatura, Estas bajo de peso  consulta a un profesional de la salud.`;            
+            }
         }else{
             document.getElementById("result").style.color="red";
-            document.getElementById("result").textContent = "Ingresa valor valido";
+            document.getElementById("result").textContent = "Ingresa valor correspondientes";
         }
 }
--- Proyectos-JavaScript/Profile/js/script.js
+++ Proyectos-JavaScript/Profile/js/script.js
@@ -1,17 +1,17 @@
-const data={
-name:"Enrique Juarez Mendoza",
-desc:"Programador en Backend y fronted, Amante de la Musica Rock",
-email:"kikeparki@gmail.com",
-phone:" (52) "+ 7299663509
+const data = {
+  name: "Enrique Juárez Mendoza",
+  desc: "Ingeniero en Computación, Egresado en Ingeníeria en Computación en la UAEMEX",
+  email: "kikeparkin@gmail.com",
+  phone: " (52) " + 7293552196,
+};
+
+function loadUserProfile(data) {
+  document.getElementById("userName").textContent = data.name;
+  document.getElementById("userDescripcion").textContent = data.desc;
+  document.getElementById("userEmail").textContent = data.email;
+  document.getElementById("userphone").textContent = data.phone;
 }
 
-function loadUserProfile(data){
-    document.getElementById("userName").textContent= data.name;
-    document.getElementById("userDescripcion").textContent= data.desc;
-    document.getElementById("userEmail").textContent= data.email;
-    document.getElementById("userphone").textContent= data.phone;
-}
-
-document.addEventListener("DOMContentLoaded", function(){
-    loadUserProfile(data);
-})
+document.addEventListener("DOMContentLoaded", function () {
+  loadUserProfile(data);
+});
